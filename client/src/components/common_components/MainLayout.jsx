04bf6a1eef2200ed4bd0cf08e51ae53d935d27cc
Dinesh3225 <dineshkumar200325@gmail.com>
2026-01-23 10:41:17 +0530

    Sidebar Done

--- client/src/components/common_components/MainLayout.jsx
+++ client/src/components/common_components/MainLayout.jsx
@@ -1,11 +1,19 @@
 // components/common_components/MainLayout.jsx
 import React from "react";
-import Header from "../common_components/Header";
+import LeftSidebar from "../sidebar/LeftSidebar";
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen text-gray-900">
-      <Header />
+    <div className="flex min-h-screen bg-gray-50">
+      {/* Left Sidebar */}
+      <div className="sticky top-0 h-screen">
+        <LeftSidebar />
+      </div>
+
+      {/* Main Content Area */}
+      <div className="flex-1 overflow-auto">
+        {/* Your page content will go here */}
+      </div>
     </div>
   );
 };
